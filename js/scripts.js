13f139ed139bbb00382524c6a77674f1346c4e4d
gabyperalta <37412202+gabyperalta@users.noreply.github.com>
2023-05-29 17:05:06 -0300

    Nuevos cambios

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,109 @@
 //MI JS
-// function focoFormulario()
-// {
-//     document.getElementById("nombre").focus();
-// }
 
+//Al cargar la página, cargo los paises en el select correspondiente
+$(document).ready(function(){
+$.ajax({
+    url: "https://restcountries.com/v3.1/all",
 
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+       
+      var select = $("#pais");
 
-$('#enviarFormulario').click(function() {
-    $('#nombre').val('holaaaaa');
-     });
+      for (var i = 0; i < data.length; i++) {
+        var option = $("<option>").text(data[i].name.common).val(data[i].name.common);
+        select.append(option);
+      }
+    },
+    error: function() {
+      alert("Error al obtener la lista de países");
+    }
+  });
+});
+
+//Al realizar un cambio en el select de paises, traigo las provincias que en este caso siempre traemos laas de Argentina
+$('#pais').change(function(){
+   
+    $.ajax({
+        url: "https://apis.datos.gob.ar/georef/api/provincias",
+        method: "GET",
+        success: function(data) {
+         
+          var select = $("#provincia");
+    
+          for (var i = 0; i < data.provincias.length; i++) {
+            var option = $("<option>").text(data.provincias[i].nombre).val(data.provincias[i].id);
+            select.append(option);
+          }
+        },
+        error: function() {
+          alert("Error al obtener los datos de la API");
+        }
+      });
+
+});
+
+//Al realizar un cambio en las provincias y seleccionar una, traigo las localidades correspondientes a la provincia seleccionada
+$('#provincia').change(function(){
+    var idProvincia=$(this).val();
+    $.ajax({
+        url: "https://apis.datos.gob.ar/georef/api/localidades",
+        method: "GET",
+        data: {
+            provincia:idProvincia,
+            max:1000
+        },
+        success: function(data) {
+          var select = $("#ciudad");
+            var option="<option selected>Seleccione una Ciudad</option>";
+          for (var i = 0; i < data.localidades.length; i++) {
+            option += "<option value="+data.localidades[i].id+">"+data.localidades[i].nombre+"</option>";
+            
+          }
+          select.html(option);
+        },
+        error: function() {
+          alert("Error al obtener los datos de la API");
+        }
+      });
+
+});
+
+//Al ingresar email, verifico que tenga el formato correspondiente
+$('#email').on('input',function() {
+    var email= $('#email').val();
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+        $('#email')[0].setCustomValidity("Por favor ingrese un correo electrónico válido.");
+    }
+    else
+    {
+        $('#email')[0].setCustomValidity("");
+
+    }
+});
+
+//Valido que la edad sea un número válido
+$('#edad').on('input',function() {
+    var edad= $('#edad').val();
+    console.log(edad)
+    if (edad<0 || edad>120) //valido que sea mayor a 0 y menor a 120, pero podría ser que controlando solo que sea mayor que 0 alcance
+    {
+        $('#edad')[0].setCustomValidity("Por favor ingrese una edad válida.");
+    }
+    else
+    {
+        $('#edad')[0].setCustomValidity("");
+
+    }
+});
+
+//Al enviarse los datos del formulario, muestro mensaje de envio correcto
+//NO ME FUNCIONA POR EL MOMENTO
+$('#miFormulario').submit(function(){
+    $('#envioCorrecto').attr('hidden',false);
+});
+
 //FIN DE MI JS
 
 
